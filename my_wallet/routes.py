9766c156e62bc29795e11b57b6209605763314ca
JosegCv <jgcusati23@gmail.com>
2023-07-12 02:33:05 -0400

    agregando esqueleto de paginas

--- my_wallet/routes.py
+++ my_wallet/routes.py
@@ -4,3 +4,7 @@ from flask import render_template, request, redirect, flash, url_for
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/purchase")
+def purchase():
+    return render_template("purchase.html")
